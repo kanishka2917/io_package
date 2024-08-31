ccecc269c27e36b0a4d5d3b58162065cef085e62
Kanishka <kanishkab2917@gmail.com>
2024-08-31 19:30:05 +0530

    print stream i o pacakge

---
+++ 01_print_stream_writer/src/com/bsl/printstream/PrintStreamDemoOne.java
@@ -0,0 +1,17 @@
+package com.bsl.printstream;
+
+public class PrintStreamDemoOne {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		System.out.println(i);
+		String s = "hello";
+		System.out.println(s);
+		double d = 10.99;
+		System.out.println(d);
+		System.out.println("value of is" + i);
+
+	}
+
+}
