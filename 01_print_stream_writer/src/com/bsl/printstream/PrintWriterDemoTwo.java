5999a6902b195753e1d9291e8c016295f59efa8d
Kanishka <kanishkab2917@gmail.com>
2024-08-31 20:13:12 +0530

    clean execution

--- 01_print_stream_writer/src/com/bsl/printstream/PrintWriterDemoTwo.java
+++ 01_print_stream_writer/src/com/bsl/printstream/PrintWriterDemoTwo.java
@@ -1,18 +1,18 @@
 package com.bsl.printstream;
 
-import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class PrintWriterDemoTwo {
 	public static void main(String[] args) {
 		
-		PrintWriterDemoTwo writer = new PrintWriterDemoTwo();
+		PrintWriter writer = new PrintWriter(System.out);
 		int i = 10;
 		String s = "hello";
 		double d = 10.99;
-		PrintStream writer1 = null;
-		writer1.println(i);
-		writer1.println(s);
-		writer1.println(d);
+		writer.println(i);
+		writer.println(s);
+		writer.println(d);
+		
 		
 	}
 
