c6bcbfe9f6c2d432366661b80eec1c097d5d95eb
Kanishka <kanishkab2917@gmail.com>
2025-03-03 22:00:15 +0530

    array_1d

---
+++ oop_java/src/oop_java/ArrayTest.java
@@ -0,0 +1,29 @@
+package oop_java;
+
+public class ArrayTest {
+	public static void main (Strinf args[]) {
+		int number [] = {48,39,81,3818,38};
+		int n = number.length;
+		System.out.println("given list");
+		for (int i = 0; i < n; i++)
+		{
+			System.out.println(" " + number[i]);
+		}
+		System.out.println("\n");
+		for(i = 0; i < n; i ++) {
+			for(j = i + 1; j < n; j++) {
+				if(number[i]<number[j]) {
+					int temp = number[i];
+					number [i] = number[j];
+					number[j] = temp;
+				}
+			}
+		}
+		System.out.println("sorted list");
+		for (int i = 0; i < n; i++)
+		{
+			System.out.println(" " + number[i]);
+		}
+	}
+
+}
